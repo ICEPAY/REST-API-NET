1d64706dd5b7afb0879d6389e2d9ae468e9110ca
icepaynl <simon.strijbos@icepay.com>
2015-11-20 14:15:45 +0100

    Initial commit

---
+++ Payment.cs
@@ -0,0 +1,34 @@
+﻿using IcepayRestClient.Classes.Payment;
+
+namespace IcepayRestClient
+{
+    public class Payment : ServiceBase
+    {
+        public Payment(int merchantID, string merchantSecret) : base(merchantID, merchantSecret) { }
+
+        public CheckoutResponse Checkout(CheckoutRequest request)
+        {
+            return IcepayRestClient.Classes.RestClient.SendAndReceive<CheckoutRequest, CheckoutResponse>("Payment", "Checkout", request, this.MerchantID, this.MerchantSecret);
+        }
+
+        public VaultCheckoutResponse VaultCheckout(VaultCheckoutRequest request)
+        {
+            return IcepayRestClient.Classes.RestClient.SendAndReceive<VaultCheckoutRequest, VaultCheckoutResponse>("Payment", "VaultCheckout", request, this.MerchantID, this.MerchantSecret);
+        }
+
+        public AutomaticCheckoutResponse AutomaticCheckout(AutomaticCheckoutRequest request)
+        {
+            return IcepayRestClient.Classes.RestClient.SendAndReceive<AutomaticCheckoutRequest, AutomaticCheckoutResponse>("Payment", "AutomaticCheckout", request, this.MerchantID, this.MerchantSecret);
+        }
+
+        public GetPaymentResponse GetPayment(GetPaymentRequest request)
+        {
+            return IcepayRestClient.Classes.RestClient.SendAndReceive<GetPaymentRequest, GetPaymentResponse>("Payment", "GetPayment", request, this.MerchantID, this.MerchantSecret);
+        }
+
+        public GetMyPaymentMethodsResponse GetMyPaymentMethods()
+        {
+            return IcepayRestClient.Classes.RestClient.SendAndReceive<GetMyPaymentMethodsRequest, GetMyPaymentMethodsResponse>("Payment", "GetMyPaymentMethods", new GetMyPaymentMethodsRequest(), this.MerchantID, this.MerchantSecret);
+        }
+    }
+}
